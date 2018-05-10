7d326efdf33757a763d729ccb1ebbed10cce3025
Matthieu <matthieupasserel@gmail.com>
2018-05-10 09:54:05 +0200

    Initial Commit

---
+++ Fashion CNN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fashion CNN
+//
+//  Created by Matthieu PASSEREL on 10/05/2018.
+//  Copyright © 2018 Matthieu PASSEREL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
